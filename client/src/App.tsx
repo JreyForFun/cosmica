27de0d9239b7456a29a5d93ea0511f960e272129
JreyForFun <jreyforfun@gmail.com>
2026-08-18 03:12:21 +0800

    Fix & Add: Fixed the router, layout  and added empty context tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import { LoginPage } from './pages/LoginPage'
+import { SignupPage } from './pages/SignupPage'
+import { Route, Routes } from "react-router-dom"
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <LoginPage />
+    <Routes>
+      <Route path="/" element={ <LoginPage /> }/>
+      <Route path="/auth/register" element={< SignupPage />} />
+    </Routes>
+
   )
 }
 
--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -9,6 +9,7 @@ import {
   FieldSeparator,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { Link } from 'react-router-dom'
 
 export function LoginForm({
   className,
