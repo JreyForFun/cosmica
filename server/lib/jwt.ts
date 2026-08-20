d7b1da3a25e065118ebec024b50514badeb293fc
JreyForFun <jreyforfun@gmail.com>
2026-08-21 01:52:45 +0800

    -d3 fix server and auth

---
+++ server/lib/jwt.ts
@@ -0,0 +1,19 @@
+import { AppError } from "../middlewares/error.middleware";
+import { TokenPayLoad } from "../types/user.types";
+import jwt, {SignOptions} from 'jsonwebtoken'
+
+export function signAccessToken(payload: TokenPayLoad): string {
+    const options: SignOptions = {
+        expiresIn: process.env.JWT_ACCESS_EXPIRATION as SignOptions['expiresIn']
+    }
+
+    return jwt.sign(payload, process.env.JWT_ACCESS_SECRET as string, options)
+}
+
+export function verifyAccessToken(token: string): TokenPayLoad{
+    try{
+        return jwt.verify(token, process.env.JWT_ACCESS_SECRET as string) as TokenPayLoad
+    }catch(err){
+        throw new AppError(401, "Invalid or expires access token")
+    }
+}
